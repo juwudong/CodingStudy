764d521ab281ce84fe5dbb2a46a6ba1b33f7e935
juwudong <Juwudong@DESKTOP-UP6UMN1>
2023-04-12 10:15:49 +0900

    1

--- src/HelloWorldApp.java
+++ src/HelloWorldApp.java
@@ -2,7 +2,7 @@ public class HelloWorldApp{
 	
 	public static void main(String[] args){
 		
-		System.out.println("Hello World!!");
+		System.out.println("Hello World!!234234234324");
 		int totla = 0;
 		for (int i = 0;i <= 101;i++) {
 			totla += i;
